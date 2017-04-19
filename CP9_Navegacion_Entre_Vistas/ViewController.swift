15a947af9b13d2963f656387d8604b46f5b47494
user125877 <user125877@77-240-12-5.rdns.melbourne.co.uk>
2017-04-19 12:37:50 +0100

    Initial Commit

---
+++ CP9_Navegacion_Entre_Vistas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CP9_Navegacion_Entre_Vistas
+//
+//  Created by user125877 on 19/4/17.
+//  Copyright © 2017 user125877. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
